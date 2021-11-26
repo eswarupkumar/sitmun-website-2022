2aff6d7dee507720abab5b0a3b9e7b7df4ab785c
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2021-11-26 20:23:23 +0530

    made About us responsive

--- src/components/landing/AboutUs.js
+++ src/components/landing/AboutUs.js
@@ -5,14 +5,13 @@ function AboutUs() {
     // This is about
     return (
         <div className="about-us">
-            <h2>About Us</h2>
+            <h2>ABOUT THIS EVENT</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
             sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            ut aliquip ex ea commodo consequat.</p>
         </div>
     )
 }
