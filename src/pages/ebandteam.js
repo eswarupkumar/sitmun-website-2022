70159db864923a77a12162e511ac42b1b4141ccc
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2021-11-28 20:41:10 +0530

    started with EB and Team

---
+++ src/pages/ebandteam.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/common/navbar";
+
+function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+      
+      <h1>Welcome to Mun Team page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
