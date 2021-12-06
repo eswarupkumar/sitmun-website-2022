b854572d56fbd10dbb8a51e1ca60a4918ed88c73
E Swarup Kumar <eswarupkumar1111@gmail.com>
2021-12-06 21:41:53 +0530

    UI changes in Hero page + title & icon update

--- src/components/landing/hero.js
+++ src/components/landing/hero.js
@@ -9,6 +9,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
 import Typewriter from "typewriter-effect";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
 
 // import Swiper core and required modules
 import SwiperCore, { EffectCards } from "swiper";
@@ -18,8 +21,33 @@ import SwiperCore, { EffectCards } from "swiper";
 SwiperCore.use([EffectFlip]);
 
 function HeroSection() {
+  AOS.init();
+
+  // You can also pass an optional settings object
+  // below listed default settings
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: "aos-init", // class applied after initialization
+    animatedClassName: "aos-animate", // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  });
+
   return (
-    <div className="hero_page_wrapper">
+    <div data-aos="zoom-in-up" className="hero_page_wrapper">
       {/* <img src={hero_bg} className="hero_bg" alt="hero_bg" /> */}
       <div className="hero_page_main">
         <div className="hero_title_wrapper">
