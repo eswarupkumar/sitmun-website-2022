2b86e8fe2a0a02c5263ab275ac7578a1ba00f23e
Raj Kishore Patra <rpatrasm@gmail.com>
2021-12-18 20:52:51 +0530

    card added

--- src/pages/committee.js
+++ src/pages/committee.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../css/committee.css';
+import bgimg from '../img/bgcommittee.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -10,25 +10,26 @@ function CommitteePage() {
     return (
         <>
             <div className="bg">
-                <h1>Committees</h1>
+                <img src={bgimg} alt="" />
+                <h2>Committees</h2>
                 <p>"A group who keep minutes and waste hours."</p>
             </div>
 
-            <div >
+            
+
             <Row xs={1} md={3} className="g-4">
                 {Array.from({ length: 6 }).map((_, idx) => (
                     <Col>
-                        <Card border="primary" className='card' style={{"border-radius": "20px"}}>
-                            <Card.Img src="https://source.unsplash.com/user/erondu/600x800" />
-                            <Card.Footer>
-                                <small className="text-muted">Some text here</small>
-                            </Card.Footer>
-                        </Card>
+                        <div class="card1">
+                            <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                            <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                            <div className="container">
+                                <div className="text">Hello World</div>
+                            </div>
+                        </div>
                     </Col>
                 ))}
             </Row>
-            </div>
-
         </>
     );
 }
