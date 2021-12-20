728a2e936dceeb20ec7500de90e774c959978749
Raj Kishore Patra <rpatrasm@gmail.com>
2021-12-20 20:28:36 +0530

    responsive

--- src/pages/committee.js
+++ src/pages/committee.js
@@ -1,9 +1,6 @@
 import React from "react";
 import '../css/committee.css';
 import bgimg from '../img/bgcommittee.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 
 function CommitteePage() {
@@ -11,25 +8,59 @@ function CommitteePage() {
         <>
             <div className="bg">
                 <img src={bgimg} alt="" />
-                <h2>Committees</h2>
+                <h1>Committees</h1>
                 <p>"A group who keep minutes and waste hours."</p>
             </div>
 
-            
+            <div className="flex">
+                <div class="card1">
+                    <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                    <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                    <div className="container">
+                        <div className="text">Hello World</div>
+                    </div>
+                </div>
 
-            <Row xs={1} md={3} className="g-4">
-                {Array.from({ length: 6 }).map((_, idx) => (
-                    <Col>
-                        <div class="card1">
-                            <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
-                            <div className="text1"><h3><b>SIT MUN</b></h3></div>
-                            <div className="container">
-                                <div className="text">Hello World</div>
-                            </div>
-                        </div>
-                    </Col>
-                ))}
-            </Row>
+                <div class="card1">
+                    <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                    <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                    <div className="container">
+                        <div className="text">Hello World</div>
+                    </div>
+                </div>
+
+                <div class="card1">
+                    <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                    <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                    <div className="container">
+                        <div className="text">Hello World</div>
+                    </div>
+                </div>
+
+                <div class="card1">
+                    <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                    <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                    <div className="container">
+                        <div className="text">Hello World</div>
+                    </div>
+                </div>
+
+                <div class="card1">
+                    <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                    <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                    <div className="container">
+                        <div className="text">Hello World</div>
+                    </div>
+                </div>
+
+                <div class="card1">
+                    <img src="https://source.unsplash.com/user/erondu/320x480" alt="Avatar" />
+                    <div className="text1"><h3><b>SIT MUN</b></h3></div>
+                    <div className="container">
+                        <div className="text">Hello World</div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
