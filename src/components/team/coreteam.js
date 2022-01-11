eb7b2cb7be8a7b78f8970bb7a1a655fd251fd599
E Swarup Kumar <eswarupkumar1111@gmail.com>
2022-01-11 22:13:21 +0530

    Minor updates img

--- src/components/team/coreteam.js
+++ src/components/team/coreteam.js
@@ -2,7 +2,7 @@ import Grid from "@mui/material/Grid";
 import * as React from "react";
 import { makeStyles } from "@mui/styles";
 
-import Heading from '../common/Heading';
+import Heading from "../common/Heading";
 import D_genoios from "../../img/Director General OIOS.jpeg";
 import sec_gen from "../../img/Secretary General.jpg";
 import d_gen from "../../img/Director General.jpg";
@@ -14,55 +14,53 @@ import Container from "react-bootstrap/Container";
 import CardComponent from "../common/card";
 import CardComponent2 from "../common/card2";
 
-
-
 const useStyles = makeStyles({
-    gridContainer: {
-      left: 0,
-      top: 0,
-      paddingLeft: "20px",
-      paddingRight: "20px",
-    },
-    root: {
-      minWidth: 340,
-      maxWidth: 340,
-      transition: "transform 0.15s ease-in-out",
-    },
-    cardHovered: {
-      transform: "scale3d(1.05, 1.05, 1)",
-    },
-  });
+  gridContainer: {
+    left: 0,
+    top: 0,
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
+  root: {
+    minWidth: 340,
+    maxWidth: 340,
+    transition: "transform 0.15s ease-in-out",
+  },
+  cardHovered: {
+    transform: "scale3d(1.05, 1.05, 1)",
+  },
+});
 
 function CoreTeam() {
-    const CoreTeam=[
-        {
-          name:"Isha Bharadwaj",
-          img:sec_gen,
-          desg:"Secretary General"
-        },
-        {
-          name:"Annada Gumansingh",
-          img:dsg,
-          desg:"Deputy Secretary General"
-        },
-        {
-          name:"Nikita Behera",
-          img:d_gen,
-          desg:"Director General"
-        }
-    ];
-    const CoreTeam2=[
-        {
-          name:"Soumya Kanta Panda",
-          img:cda,
-          desg:"Charge D’ Affairs"
-        },
-        {
-          name:"Sanjog Samuel Samantaray",
-          img:D_genoios,
-          desg:"Director General OIOS"
-        }
-      ];
+  const CoreTeam = [
+    {
+      name: "Isha Bharadwaj",
+      img: sec_gen,
+      desg: "Secretary General",
+    },
+    {
+      name: "Annada Gumansingh",
+      img: dsg,
+      desg: "Deputy Secretary General",
+    },
+    {
+      name: "Nikita Behera",
+      img: d_gen,
+      desg: "Director General",
+    },
+  ];
+  const CoreTeam2 = [
+    {
+      name: "Soumya Kanta Panda",
+      img: cda,
+      desg: "Charge D’ Affairs",
+    },
+    {
+      name: "Sanjog Samuel Samantaray",
+      img: D_genoios,
+      desg: "Director General OIOS",
+    },
+  ];
   const classes = useStyles();
   const clickMe = (event) => {
     console.log(event);
@@ -71,25 +69,21 @@ function CoreTeam() {
     raised: false,
     shadow: 1,
   });
-    return (
-        <div>
-            <Heading head="Core Team" />
-            <Container fluid m={4} p={4}>
-            <Grid container spacing={6} className={classes.gridContainer}>
-             
-             <CardComponent details={CoreTeam}/>  
-             
-           </Grid>
-            <Container fluid m={4}>
-                    <Grid container spacing={6} className={classes.gridContainer}>
-                        <CardComponent2 details={CoreTeam2}/>  
-                    </Grid>
-                </Container>
-                
-            </Container>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Heading head="Core Team" />
+      <Container fluid m={4} p={4}>
+        <Grid container spacing={6} className={classes.gridContainer}>
+          <CardComponent details={CoreTeam} />
+        </Grid>
+        <Container fluid m={4}>
+          <Grid container spacing={6} className={classes.gridContainer}>
+            <CardComponent2 details={CoreTeam2} />
+          </Grid>
+        </Container>
+      </Container>
+    </div>
+  );
 }
 
 export default CoreTeam;
