bf01f16b908e8c328a6bfdab5dbc292f797d964f
E Swarup Kumar <eswarupkumar1111@gmail.com>
2022-02-26 22:19:10 +0530

    about-us incomplete update

--- src/components/landing/AboutUs.js
+++ src/components/landing/AboutUs.js
@@ -9,20 +9,37 @@ function AboutUs() {
   return (
     <Container className="about-us" id="about">
       <Heading head="ABOUT THIS EVENT" />
-      <div className="about-us-div">
+      <div className="about-us-wrapper">
         <img src={aboutImg} alt="about-event" data-aos="fade-down" />
-        <p>
-          Silicon Institute Of Technology Model United Nations (SITMUN) is a
-          simulation of the United Nations where students delegate on behalf of
-          member countries to find solutions to the rising global issues through
-          research, drafting, lobbying and debating. Students can also
-          participate as members of the International Press (as
-          reporters/photographers). SITMUN has always been a unique
-          conglomeration of learning and experiencing a MUN, and this time
-          again, we are back with an unparalleled edition. Not even a pandemic
-          can stop those who dare, and this 11th-13th March, we aim at bringing
-          revolution through resolution. We are in, are you!?
-        </p>
+        <div className="about-us-content">
+          <p>
+            Silicon Institute Of Technology Model United Nations (SITMUN) is a
+            simulation of the United Nations where students delegate on behalf
+            of member countries to find solutions to the rising global issues
+            through research, drafting, lobbying and debating. Students can also
+            participate as members of the International Press (as
+            reporters/photographers). SITMUN has always been a unique
+            conglomeration of learning and experiencing a MUN, and this time
+            again, we are back with an unparalleled edition. Not even a pandemic
+            can stop those who dare, and this 11th-13th March, we aim at
+            bringing revolution through resolution. We are in, are you!?
+          </p>
+          <div className="info-links">
+            <div className="btn-section">
+              <a href="/#about" className="hero-button">
+                Know More
+              </a>
+              <a
+                href="https://rb.gy/s1uypm"
+                target="_blank"
+                rel="noreferrer"
+                className="hero-button"
+              >
+                Register
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </Container>
   );
